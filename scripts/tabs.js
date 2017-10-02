c835e2dd5cb5572d504e1fb004864666340c6a6d
John Lemuel Suarez <arcaz.johnlns@gmail.com>
2017-10-02 22:12:11 +0800

    Minor fixes on positions and errors

---
+++ scripts/tabs.js
@@ -0,0 +1,15 @@
+function openAbout(cityName, elmnt, color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(cityName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+
+}
+document.getElementById("defaultOpen").click();
